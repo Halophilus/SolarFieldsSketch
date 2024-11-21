a00375e409526c2169b14eba8fd1500502a45226
Ben Henshaw <33325217+Halophilus@users.noreply.github.com>
2024-11-21 10:39:40 -0500

    UpdateEntryPanel

--- src/EntryPanel.java
+++ src/EntryPanel.java
@@ -3,7 +3,7 @@ import java.awt.*;
 import java.time.LocalDate;
 import java.util.UUID;
 
-public class EntryPanel {
+public class EntryPanel implements Comparable<EntryPanel> {
     // Determines whether the frame is from intro or edit section
     boolean isIntro;
 
@@ -24,6 +24,7 @@ public class EntryPanel {
 
     // Labels
     public JLabel idHeader;
+    public JLabel datePostedHeader;
     public JLabel imageLabel;
     public JLabel descriptionLabel;
     public JLabel entryLabel;
@@ -37,20 +38,23 @@ public class EntryPanel {
         this.reviewed = reviewed;
         this.isIntro = isIntro;
 
-
-
         // Stores all entry data
-        outerPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0));
+        outerPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 5));
 
         // Creates space for Entry ID and Icon+Image
         stackedPanel = new JPanel();
         BoxLayout boxLayout = new BoxLayout(stackedPanel, BoxLayout.Y_AXIS);
         stackedPanel.setLayout(boxLayout);
 
-        // Creates space for entry ID header
-        idPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 5));
+        // Creates space for entry ID + date header
+        idPanel = new JPanel(new BorderLayout());
         // Create ID label
-        
+        idHeader = new JLabel(entryId.toString());
+        // Create date panel
+        datePostedHeader = new JLabel(datePosted.toString());
+        // Add it to iDPanel
+        idPanel.add(BorderLayout.WEST, idHeader);
+        idPanel.add(BorderLayout.EAST, datePostedHeader);
 
         // Creates space for Icon + Image
         imageAndDescriptionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0));
@@ -61,49 +65,12 @@ public class EntryPanel {
         imageAndDescriptionPanel.add(imageLabel);
         imageAndDescriptionPanel.add(descriptionLabel);
 
+        // Add subpanels to stackedPanel
+        stackedPanel.add(idPanel);
+        stackedPanel.add(imageAndDescriptionPanel);
 
-        // Entry label
-        entryLabel = new JLabel(image);
-        entryLabel.setText(description);
-        // ID panel
-        // Stores all identifying info about a ticket
-        idPanel = new JPanel();
-        //idPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 0)); //; align left, horizontal gap of 5 pixels
-
-        // The ticket number
-        idLabel = new JLabel("#" + this.ticketId);
-        //idLabel.setFont(new Font("Arial", Font.BOLD, 12));
-        idLabel.setForeground(Color.BLUE);
-
-        // The divider between them
-        dividerLabel = new JLabel("|");
-        //dividerLabel.setFont(new Font("Arial", Font.BOLD, 12));
-
-        // The number of entries
-        entriesLabel = new JLabel("No. Entries: " + this.numEntries);
-        //entriesLabel.setFont(new Font("Arial", Font.ITALIC, ));
-
-        // Font color as a visual indicator of unresolved issues
-        if (!resolved){ // If the issues are unresolved
-            entriesLabel.setForeground(Color.RED);
-        } else{
-            entriesLabel.setForeground(Color.DARK_GRAY);
-
-        }
-
-        // Adding the labels to the idPanel
-        idPanel.add(idLabel);
-        idPanel.add(dividerLabel);
-        idPanel.add(entriesLabel);
-
-        dateLabel = new JLabel("Last edited: " + mostRecentEditDate);
-
-        //panel.setPreferredSize(new Dimension(100, idPanel.getHeight()));
-        outerPanel.add(idPanel, BorderLayout.WEST);
-        outerPanel.add(dateLabel, BorderLayout.EAST);
-
-
-
+        // Add stackedPanel to outerPanel
+        outerPanel.add(stackedPanel);
 
     }
 
@@ -117,10 +84,10 @@ public class EntryPanel {
     }
 
     @Override
-    public int compareTo(TicketPanel otherTicket) {
-        if (this.mostRecentEditDate.isBefore(otherTicket.mostRecentEditDate)){
+    public int compareTo(EntryPanel otherEntry) {
+        if (this.datePosted.isBefore(otherEntry.datePosted)) {
             return 1; // If the current date is before the provided date
-        } else if (this.mostRecentEditDate.isAfter(otherTicket.mostRecentEditDate)){
+        } else if (this.datePosted.isAfter(otherEntry.datePosted)) {
             return -1;
         } else {
             return 0;
